45a36d52ff793130667a289d5d3c63e3e69816ce
Adam Bourg <Adam.bourg@gmail.com>
2018-09-11 00:40:06 -0600

    adding tests

---
+++ src/components/Leaderboard/Leaderboard.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Leaderboad from './index';
+
+// to do
+describe('Leaderboad Tests', () => {
+
+  it('ensures that the board is sorted', () => {
+  });
+
+  it('ensures that the board the correct players on the board', () => {
+  });
+
+  it('ensures that the delete button works', () => {
+  });
+
+});
--- src/components/Leaderboard/children/AddPlayer/AppPlayer.test.js
+++ src/components/Leaderboard/children/AddPlayer/AppPlayer.test.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
+import AddPlayer from './index';
 
 describe('AddPlayer Tests', () => {
 
+  it('ensuring my on change method gets called', () => {
+    const spy = jest.fn();
+    const form = shallow(<AddPlayer formSubmit={spy} />);
+
+    expect(spy).not.toBeCalled();
+
+    form.find('input').simulate('change');
+
+    expect(spy).toBeCalled();
+  });
+
+  it('ensures that the add player button fails when there is not data', () => {
+  });
+
+  it('ensures that the add player button works nicely', () => {
+  });
 });
